aa8cc5f390245e250cf2a0f1e9f91de1128f4fd0
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-27 15:38:15 +0100

    check for access of devicemotion

--- sensors-testpage/script.js
+++ sensors-testpage/script.js
@@ -1,3 +1,25 @@
+addEventListener("load", (event) => {
+    let deviceMotionAvailable = document.getElementById("device-motion-available");
+    if (typeof DeviceMotionEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceMotionEvent.requestPermission()
+            .then((state) => {
+                if (state === 'granted') {
+                    window.addEventListener('devicemotion', handleOrientation);
+                } else {
+                    deviceMotionAvailable.innerHTML = "Request to access the orientation was rejected";
+                }
+            })
+            .catch(console.error);
+    } else {
+        // Handle regular non iOS 13+ devices.
+        deviceMotionAvailable.innerHTML = "DeviceMotion is available";
+        window.addEventListener('devicemotion', handleOrientation);
+    }
+});
+
+
+
 addEventListener("devicemotion", (event) => {
     let accelerationSpan = document.getElementById("acceleration");
     accelerationSpan.innerHTML = `x: ${event.acceleration.x} y: ${event.acceleration.y} z: ${event.acceleration.z}`;
