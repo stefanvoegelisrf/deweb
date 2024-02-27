c809f6f4c9b9cc8a7f95d380fcfde7265118adab
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-02-27 16:55:10 +0100

    adjust permission code

--- sensors-testpage/script.js
+++ sensors-testpage/script.js
@@ -1,5 +1,11 @@
 addEventListener("load", (event) => {
-    let deviceMotionAvailable = document.getElementById("device-motion-available");
+    let deviceMotionButton = document.getElementById("device-motion-button");
+    deviceMotionButton.addEventListener("click", onDeviceMotionButtonClick);
+    let deviceOrientationButton = document.getElementById("device-orientation-button");
+    deviceOrientationButton.addEventListener("click", onDeviceOrientationButtonClick);
+});
+
+function onDeviceMotionButtonClick() {
     if (typeof DeviceMotionEvent.requestPermission === 'function') {
         // Handle iOS 13+ devices.
         DeviceMotionEvent.requestPermission()
@@ -7,26 +13,40 @@ addEventListener("load", (event) => {
                 if (state === 'granted') {
                     window.addEventListener('devicemotion', handleMotion);
                 } else {
-                    deviceMotionAvailable.innerHTML = "Request to access the orientation was rejected";
+                    console.error('Request to access the motion was rejected');
                 }
             })
             .catch(console.error);
     } else {
         // Handle regular non iOS 13+ devices.
-        deviceMotionAvailable.innerHTML = "DeviceMotion is available";
         window.addEventListener('devicemotion', handleMotion);
     }
-
-    window.addEventListener('deviceorientation', handleOrientation);
-});
-
-function handleMotion(event) {
-    let accelerationSpan = document.getElementById("acceleration");
-    console.log(event);
-    accelerationSpan.innerHTML = `x: ${event.acceleration.x} y: ${event.acceleration.y} z: ${event.acceleration.z}`;
 }
 
-function handleOrientation(event){
-    let orientationSpan = document.getElementById("orientation");
-    orientationSpan.innerHTML = `alpha: ${event.alpha} beta: ${event.beta} gamma: ${event.gamma}`;
+function onDeviceOrientationButtonClick() {
+    if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+        // Handle iOS 13+ devices.
+        DeviceOrientationEvent.requestPermission()
+            .then((state) => {
+                if (state === 'granted') {
+                    window.addEventListener('deviceorientation', handleOrientation);
+                } else {
+                    console.error('Request to access the orientation was rejected');
+                }
+            })
+            .catch(console.error);
+    } else {
+        // Handle regular non iOS 13+ devices.
+        window.addEventListener('deviceorientation', handleOrientation);
+    }
+}
+
+function handleMotion(event) {
+    let motionElement = document.getElementById("motion");
+    motionElement.innerHTML = `Acceleration: x: ${event.acceleration.x}, y: ${event.acceleration.y}, z: ${event.acceleration.z}`;
+}
+
+function handleOrientation(event) {
+    let orientationElement = document.getElementById("orientation");
+    orientationElement.innerHTML = `Orientation: alpha: ${event.alpha}, beta: ${event.beta} ${event.gamma}`;
 }
