9c7400b89c958d5d6955151a1fd09b23b7a42c96
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-04 17:37:48 +0100

    add interpolation for rotation

--- gradient-wheel/script.js
+++ gradient-wheel/script.js
@@ -1,12 +1,42 @@
-window.onload = function() {
-    let rotationSlider= document.getElementById('rotation-slider');
-    rotationSlider.addEventListener('input', onRotationChanged)
+let previousGradientRotationValue = 0;
+let gradientCircleCollection = [];
+window.onload = function () {
+    let rotationSlider = document.getElementById('rotation-slider');
+    rotationSlider.addEventListener('input', onRotationChanged);
+    let gradientCircles = document.getElementsByClassName('gradient-circle');
+    let gradientCirclesArray = Array.from(gradientCircles);
+    gradientCirclesArray = gradientCirclesArray.reverse();
+    for (let gradientCirle of gradientCircles) {
+        gradientCircleCollection.push(new GradientCircle(gradientCirle));
+    }
+    setInterval(updateGradientCircles, 10)
 };
 
 function onRotationChanged(event) {
     let rotation = event.target.value;
-    let gradientCircles = document.getElementsByClassName('gradient-circle');
-    for(let gradientCirle of gradientCircles) {
-        gradientCirle.style.rotate = `${rotation}deg`
+    previousGradientRotationValue = rotation;
+    console.log(rotation);
+}
+
+function updateGradientCircles() {
+    for (let i = 0; i < gradientCircleCollection.length; i++) {
+        gradientCircleCollection[i].rotation = lerp(gradientCircleCollection[i].rotation, previousGradientRotationValue, (i + 1) * 0.005);
+        gradientCircleCollection[i].update();
+    }
+}
+
+function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end;
+}
+
+class GradientCircle {
+    element;
+    rotation = 0;
+    constructor(element) {
+        this.element = element;
+    }
+    update() {
+        console.log(this.rotation);
+        this.element.style.rotate = `${this.rotation}deg`;
     }
 }
