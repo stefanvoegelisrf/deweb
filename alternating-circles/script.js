54dd5af8dc353d042ba381b30e4662c630d3b5ff
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-04 13:27:47 +0100

    add circles, refine, add control icons

--- alternating-circles/script.js
+++ alternating-circles/script.js
@@ -1,17 +1,38 @@
+let currentBgColor = "circle-bg-color-1";
+let circleCount = 0;
+let maxCircleCount = 18;
+let addCircleInterval;
 window.onload = function () {
+    setTimeout(function () {
+        addCircleInterval = setInterval(addInitialCirclesInterval, 200);
+    }, 500);
+    let alternatingCirclesContainer = document.getElementById("alternating-circles-container");
+    alternatingCirclesContainer.addEventListener("click", addCircleToContainer);
+}
+
+function addCircleToContainer() {
     let alternatingCirclesContainer = document.getElementById("alternating-circles-container");
     let firstCircle = document.createElement("div");
-    firstCircle.classList.add("alternating-circles-box");
+    firstCircle.classList.add("alternating-circles-box", "rgb", currentBgColor);
     alternatingCirclesContainer.appendChild(firstCircle);
-    setTimeout(() => {
-        let secondCircle = document.createElement("div");
-        secondCircle.classList.add("alternating-circles-box");
-        alternatingCirclesContainer.appendChild(secondCircle);
-    }, 1000);
-    setTimeout(() => {
-        let thirdCircle = document.createElement("div");
-        thirdCircle.classList.add("alternating-circles-box");
-        alternatingCirclesContainer.appendChild(thirdCircle);
-    }, 1500);
+    nextBgColor();
+}
 
+function addInitialCirclesInterval() {
+    addCircleToContainer();
+    circleCount++;
+    if (circleCount >= maxCircleCount) {
+        clearInterval(addCircleInterval);
+    }
+}
+
+function nextBgColor() {
+    if (currentBgColor === "circle-bg-color-1") {
+        currentBgColor = "circle-bg-color-2";
+    } else if (currentBgColor === "circle-bg-color-2") {
+        currentBgColor = "circle-bg-color-3";
+    }
+    else if (currentBgColor === "circle-bg-color-3") {
+        currentBgColor = "circle-bg-color-1";
+    }
 }
