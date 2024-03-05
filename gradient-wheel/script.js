28a10bfea4a76755a7379869833a8986e71e0b8b
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-05 08:51:40 +0100

    add radial slider

--- gradient-wheel/script.js
+++ gradient-wheel/script.js
@@ -1,8 +1,8 @@
 let previousGradientRotationValue = 0;
 let gradientCircleCollection = [];
+let isDraggingRadialSlider = false;
+
 window.onload = function () {
-    let rotationSlider = document.getElementById('rotation-slider');
-    rotationSlider.addEventListener('input', onRotationChanged);
     let gradientCircles = document.getElementsByClassName('gradient-circle');
     let gradientCirclesArray = Array.from(gradientCircles);
     gradientCirclesArray = gradientCirclesArray.reverse();
@@ -10,12 +10,53 @@ window.onload = function () {
         gradientCircleCollection.push(new GradientCircle(gradientCirle));
     }
     setInterval(updateGradientCircles, 10)
+    let radialSlider = document.getElementById('radial-slider');
+    radialSlider.addEventListener('mousedown', function (event) {
+        isDraggingRadialSlider = true;
+    });
+    radialSlider.addEventListener('touchstart', function (event) {
+        isDraggingRadialSlider = true;
+    });
+    document.addEventListener('mouseup', function (event) {
+        isDraggingRadialSlider = false;
+    });
+    document.addEventListener('mouseup', function (event) {
+        isDraggingRadialSlider = false;
+    });
+    window.addEventListener('mousemove', onDragSlider);
+    window.addEventListener('touchmove', onDragSlider);
 };
 
-function onRotationChanged(event) {
-    let rotation = event.target.value;
+function onDragSlider(event) {
+    let angle, centerX, centerY, radialSlider, deltaX, deltaY, positionX, positionY, touch, boundingRect;
+    if (isDraggingRadialSlider) {
+        radialSlider = document.getElementById('radial-slider');
+        touch = void 0;
+        if (event.touches) {
+            touch = event.touches[0];
+        }
+        boundingRect = radialSlider.getBoundingClientRect();
+        centerX = (boundingRect.width * 0.5) + boundingRect.left;
+        centerY = (boundingRect.height * 0.5) + boundingRect.top;
+        positionX = event.pageX || touch.pageX;
+        positionY = event.pageY || touch.pageY;
+        deltaY = centerY - positionY;
+        deltaX = centerX - positionX;
+        // Calculate Angle between circle center and mouse position
+        angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+        angle -= 90;
+        if (angle < 0) {
+            angle = 360 + angle; // Always show angle positive
+        }
+        angle = Math.round(angle);
+        onRotationChanged(angle);
+        let radialSliderHandle = document.getElementById("radial-slider-handle");
+        radialSliderHandle.style.rotate = `${angle}deg`;
+    }
+}
+
+function onRotationChanged(rotation) {
     previousGradientRotationValue = rotation;
-    console.log(rotation);
 }
 
 function updateGradientCircles() {
