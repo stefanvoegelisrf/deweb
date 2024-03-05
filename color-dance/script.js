ec097fa855851f4e34fa4c05f3d62bbdc539d7ba
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-05 13:33:47 +0100

    change layout to grid

--- color-dance/script.js
+++ color-dance/script.js
@@ -35,6 +35,21 @@ function changeActiveColorCombo(combo) {
             circleBox.classList.add("ppy");
         }
     }
+    let gradientCircles = document.getElementsByClassName('gradient-circle');
+    for (let gradientCircle of gradientCircles) {
+        gradientCircle.classList.remove("gradient-circle-cmy");
+        gradientCircle.classList.remove("gradient-circle-rgb");
+        gradientCircle.classList.remove("gradient-circle-ppy");
+        if (combo === "color-combo-1") {
+            gradientCircle.classList.add("gradient-circle-cmy");
+        }
+        else if (combo === "color-combo-2") {
+            gradientCircle.classList.add("gradient-circle-rgb");
+        }
+        else if (combo === "color-combo-3") {
+            gradientCircle.classList.add("gradient-circle-ppy");
+        }
+    }
 }
 
 function changeClassesOfCircles(circle1Class, circle2Class, circle3Class) {
@@ -56,7 +71,6 @@ function changeClassesOfCircles(circle1Class, circle2Class, circle3Class) {
     circle2.classList.add(circle2Class);
     circle3.classList.remove(circle3.classList[1]);
     circle3.classList.add(circle3Class);
-
 }
 
 let circleBoxRed, circleBoxGreen, circleBoxBlue;
@@ -92,7 +106,8 @@ addEventListener("load", (event) => {
         switchToTouchMode();
     }
     else {
-        addEventListener("mousemove", handleMouseMove);
+        let pendulumContainer = document.getElementById("pendulum-container");
+        pendulumContainer.addEventListener("mousemove", handlePendulumMouseMove);
         addUpdateInterval();
     }
 
@@ -103,6 +118,29 @@ addEventListener("load", (event) => {
     addButton.addEventListener("click", addCircleToContainer);
     let removeButton = document.getElementById("remove-button");
     removeButton.addEventListener("click", removeCircleFromContainer);
+
+    let gradientCircles = document.getElementsByClassName('gradient-circle');
+    let gradientCirclesArray = Array.from(gradientCircles);
+    gradientCirclesArray = gradientCirclesArray.reverse();
+    for (let gradientCirle of gradientCircles) {
+        gradientCircleCollection.push(new GradientCircle(gradientCirle));
+    }
+    setInterval(updateGradientCircles, 10)
+    let radialSlider = document.getElementById('radial-slider');
+    radialSlider.addEventListener('mousedown', function (event) {
+        isDraggingRadialSlider = true;
+    });
+    radialSlider.addEventListener('touchstart', function (event) {
+        isDraggingRadialSlider = true;
+    });
+    document.addEventListener('mouseup', function (event) {
+        isDraggingRadialSlider = false;
+    });
+    document.addEventListener('mouseup', function (event) {
+        isDraggingRadialSlider = false;
+    });
+    window.addEventListener('mousemove', onDragSlider);
+    window.addEventListener('touchmove', onDragSlider);
 });
 
 function addUpdateInterval() {
@@ -139,7 +177,7 @@ function handleOrientation(event) {
     });
 }
 
-function handleMouseMove(event) {
+function handlePendulumMouseMove(event) {
     handleXChange(event.clientX);
 }
 
@@ -188,7 +226,11 @@ function switchMode(mode) {
 }
 
 function handleXChange(x) {
-    let width = window.innerWidth;
+    let pendulumContainer = document.getElementById("pendulum-container");
+    let pendulumContainerBoundingRect = pendulumContainer.getBoundingClientRect();
+    let width = pendulumContainerBoundingRect.width;
+    x = x - pendulumContainerBoundingRect.left
+    console.log(width)
     let rotateValue = (x / width) * 180 - 90;
     previousRotateValue = rotateValue;
     updateCircleBoxes(rotateValue, 0.2);
@@ -258,5 +300,83 @@ function nextBgColor() {
     }
     else if (currentBgColor === "circle-bg-color-3") {
         currentBgColor = "circle-bg-color-1";
+    }
+}
+
+
+let previousGradientRotation = 0;
+let previousGradientRotationTotal = 0;
+let gradientCircleCollection = [];
+let isDraggingRadialSlider = false;
+
+function onDragSlider(event) {
+    if (isDraggingRadialSlider) {
+        let angle,
+            centerX,
+            centerY,
+            radialSlider,
+            deltaX,
+            deltaY,
+            positionX,
+            positionY,
+            touch,
+            boundingRect;
+        radialSlider = document.getElementById('radial-slider');
+        touch = void 0;
+        if (event.touches) {
+            touch = event.touches[0];
+        }
+        boundingRect = radialSlider.getBoundingClientRect();
+        centerX = (boundingRect.width * 0.5) + boundingRect.left;
+        centerY = (boundingRect.height * 0.5) + boundingRect.top;
+        positionX = (event.pageX || touch.pageX);
+        positionY = (event.pageY || touch.pageY);
+        console.log(`position: ${positionX}, ${positionY}, center: ${centerX}, ${centerY}`)
+        deltaY = centerY - positionY;
+        deltaX = centerX - positionX;
+        // Calculate Angle between circle center and mouse position
+        angle = Math.atan2(deltaY, deltaX) * (180 / Math.PI);
+        angle -= 90;
+        if (angle < 0) {
+            angle = 360 + angle; // Always show angle positive
+        }
+        angle = Math.round(angle);
+        onRotationChanged(angle);
+        let radialSliderHandle = document.getElementById("radial-slider-handle");
+        radialSliderHandle.style.rotate = `${angle}deg`;
+    }
+}
+
+function onRotationChanged(rotation) {
+    let differenceToPrevious = rotation - previousGradientRotation;
+    if (differenceToPrevious > 300) {
+        differenceToPrevious = 360 - differenceToPrevious;
+    }
+    else if (differenceToPrevious < -300) {
+        differenceToPrevious = 360 + differenceToPrevious;
+    }
+    previousGradientRotation = rotation;
+    previousGradientRotationTotal += differenceToPrevious;
+}
+
+function updateGradientCircles() {
+    for (let i = 0; i < gradientCircleCollection.length; i++) {
+        gradientCircleCollection[i].rotation = lerp(gradientCircleCollection[i].rotation, previousGradientRotationTotal, (i + 1) * 0.01);
+        gradientCircleCollection[i].update();
+    }
+}
+
+function lerp(start, end, amt) {
+    return (1 - amt) * start + amt * end;
+}
+
+class GradientCircle {
+    element;
+    rotation = 0;
+    constructor(element) {
+        this.element = element;
+    }
+    update() {
+        this.element.style.rotate = `${this.rotation}deg`;
     }
 }
