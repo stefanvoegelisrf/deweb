69d3adf4f0e74918b65662f815c749e4adf2cdce
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-12 11:29:44 +0100

    add menu to pages, add stylesheet, add shadowdom

--- process-menu/process-menu.js
+++ process-menu/process-menu.js
@@ -1,10 +1,56 @@
 class ProcessMenu extends HTMLElement {
+    shadowRoot;
     constructor() {
         super();
+        this.shadowRoot = this.attachShadow({ mode: "closed" });
         const font = document.createElement("link");
         font.href = "https://fonts.googleapis.com/css2?family=Unbounded&display=swap";
         font.rel = "stylesheet"
         document.head.appendChild(font);
+
+        const iconFont = document.createElement("link");
+        iconFont.href = "https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
+        iconFont.rel = "stylesheet"
+        document.head.appendChild(iconFont);
+
+        const styleSheet = new CSSStyleSheet();
+        styleSheet.replaceSync(`
+        *{
+            font-family: 'Unbounded', sans-serif;
+            font-size: 16px;
+        }
+        .material-icons-outlined {
+            font-variation-settings: 'FILL' 0,'wght' 400,'GRAD' 0,'opsz' 24;
+            font-family: 'Material Symbols Outlined';
+            font-weight: normal;
+            font-style: normal;
+            font-size: 24px;
+            line-height: 1;
+            letter-spacing: normal;
+            text-transform: none;
+            display: inline-block;
+            white-space: nowrap;
+            word-wrap: normal;
+            direction: ltr;
+            -webkit-font-feature-settings: 'liga';
+            -webkit-font-smoothing: antialiased;
+        }
+        .menu{
+            position: fixed;
+            top: 50%;
+            transform: translateY(-50%);
+            left: 0;
+            background-color: red;
+            padding: 10px;
+            border-radius: 10px;
+            text-align: center;
+        }
+        ul{
+            list-style: none;
+            padding: 0;
+        }
+        `)
+        this.shadowRoot.adoptedStyleSheets = [styleSheet];
     }
     // The browser calls this method when the element is
     // added to the DOM.
@@ -15,20 +61,15 @@ class ProcessMenu extends HTMLElement {
         } else {
             baseUrl = "/kiweb";
         }
-        const now = new Date();
-        this.style.fontFamily = "Unbounded, sans-serif";
         let menuDiv = document.createElement('div');
-        menuDiv.innerHTML = `Menu`;
-        menuDiv.style.position = 'fixed';
-        menuDiv.style.top = '50%';
-        menuDiv.style.transform = 'translateY(-50%)';
-        menuDiv.style.left = '0';
-        menuDiv.style.backgroundColor = 'red';
-        menuDiv.style.padding = '10px';
-        menuDiv.style.borderRadius = '10px';
+        menuDiv.classList.add('menu');
+
+        let menuIconSpan = document.createElement('span');
+        menuIconSpan.classList.add('material-icons-outlined');
+        menuIconSpan.innerHTML = "menu";
+        menuDiv.appendChild(menuIconSpan);
+
         let linkList = document.createElement('ul');
-        linkList.style.listStyle = 'none';
-        linkList.style.padding = '0';
 
         let listElementBrockmann = document.createElement('li');
         let linkBrockmann = document.createElement('a');
@@ -43,8 +84,58 @@ class ProcessMenu extends HTMLElement {
         linkMikeJoyce.innerHTML = "Mike Joyce";
         listElementMikeJoyce.appendChild(linkMikeJoyce);
         linkList.appendChild(listElementMikeJoyce);
+
+        let listElementPendulumSwing = document.createElement('li');
+        let linkPendulumSwing = document.createElement('a');
+        linkPendulumSwing.href = `${baseUrl}/pendulum-swing-with-motion/`;
+        linkPendulumSwing.innerHTML = "Pendulum";
+        listElementPendulumSwing.appendChild(linkPendulumSwing);
+        linkList.appendChild(listElementPendulumSwing);
+
+        let listElementAlternatingCircles = document.createElement('li');
+        let linkAlternatingCircles = document.createElement('a');
+        linkAlternatingCircles.href = `${baseUrl}/alternating-circles/`;
+        linkAlternatingCircles.innerHTML = "Alternating Circles";
+        listElementAlternatingCircles.appendChild(linkAlternatingCircles);
+        linkList.appendChild(listElementAlternatingCircles);
+
+        let listElementMatterJsIntroduction = document.createElement('li');
+        let linkMatterJsIntroduction = document.createElement('a');
+        linkMatterJsIntroduction.href = `${baseUrl}/codingtrain-matterjs-introduction/`;
+        linkMatterJsIntroduction.innerHTML = "Matter.js";
+        listElementMatterJsIntroduction.appendChild(linkMatterJsIntroduction);
+        linkList.appendChild(listElementMatterJsIntroduction);
+
+        let listElementGradientWheel = document.createElement('li');
+        let linkGradientWheel = document.createElement('a');
+        linkGradientWheel.href = `${baseUrl}/gradient-wheel/`;
+        linkGradientWheel.innerHTML = "Gradient Wheel";
+        listElementGradientWheel.appendChild(linkGradientWheel);
+        linkList.appendChild(listElementGradientWheel);
+
+        let listElementColorDance = document.createElement('li');
+        let linkColorDance = document.createElement('a');
+        linkColorDance.href = `${baseUrl}/color-dance/`;
+        linkColorDance.innerHTML = "Color Dance";
+        listElementColorDance.appendChild(linkColorDance);
+        linkList.appendChild(listElementColorDance);
+
+        let listElementQuirkySkateboard = document.createElement('li');
+        let linkQuirkySkateboard = document.createElement('a');
+        linkQuirkySkateboard.href = `${baseUrl}/skate-scrolly/`;
+        linkQuirkySkateboard.innerHTML = "Skate";
+        listElementQuirkySkateboard.appendChild(linkQuirkySkateboard);
+        linkList.appendChild(listElementQuirkySkateboard);
+
+        let listElementGradientColorChange = document.createElement('li');
+        let linkGradientColorChange = document.createElement('a');
+        linkGradientColorChange.href = `${baseUrl}/gradient-color-change/`;
+        linkGradientColorChange.innerHTML = "Gradient Color";
+        listElementGradientColorChange.appendChild(linkGradientColorChange);
+        linkList.appendChild(listElementGradientColorChange);
+
         menuDiv.appendChild(linkList);
-        this.appendChild(menuDiv);
+        this.shadowRoot.appendChild(menuDiv);
     }
 }
 
