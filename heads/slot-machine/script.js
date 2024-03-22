313710854be40341efb43b672dbb984da823c021
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-22 10:51:34 +0100

    slot machine initial

---
+++ heads/slot-machine/script.js
@@ -0,0 +1,30 @@
+let spinning = [false, false, false]; // Spinning state for each slot
+let isSlotMachineRunning = false;
+
+window.onload = function () {
+    const slotTemplate = document.getElementById('slot-template');
+    const slot1 = document.getElementById('slot-1');
+    const slot2 = document.getElementById('slot-2');
+    const slot3 = document.getElementById('slot-3');
+    slot1.appendChild(slotTemplate.content.cloneNode(true));
+    slot2.appendChild(slotTemplate.content.cloneNode(true));
+    slot3.appendChild(slotTemplate.content.cloneNode(true));
+}
+
+const slots = document.querySelectorAll('.slot');
+
+function startSpin(slotIndex) {
+  if (!spinning[slotIndex]) {
+    slots[slotIndex].classList.add('spin');
+    spinning[slotIndex] = true;
+  }
+}
+
+function stopSpin(slotIndex) {
+  if (spinning[slotIndex]) {
+    slots[slotIndex].addEventListener('animationiteration', () => {
+      slots[slotIndex].classList.remove('spin');
+      spinning[slotIndex] = false;
+    }, { once: true });
+  }
+}
