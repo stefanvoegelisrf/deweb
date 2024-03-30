13c52fd27aff076a8361afdaf6ab50ce5019a7ef
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-03-30 13:35:26 +0100

    simplify array initialization

--- effects/flashing-light-array/script.js
+++ effects/flashing-light-array/script.js
@@ -1,6 +1,9 @@
 class LightSettings {
     id;
     element;
+    constructor(element) {
+        this.element = element;
+    }
     trigger(iterationCount, duration, delay = 0, animationName = "flashing-light") {
         setTimeout(() => {
             this.element.style.animationIterationCount = `${iterationCount}`;
@@ -16,49 +19,36 @@ class LightSettings {
 }
 
 class LightArray {
-    state = [
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()],
-        [new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings(), new LightSettings()]
-    ];
-    constructor(containerId = "light-container") {
+    constructor(containerId, arrayRows = 10, arrayColumns = 10) {
+        this.state = [];
         const lightContainer = document.getElementById(containerId);
-        let lightRowIndex = 0;
-        for (let lightRow of this.state) {
-            let lightColumnIndex = 0;
-            for (let lightColumn of lightRow) {
+        lightContainer.style.display = "grid";
+        lightContainer.style.gridTemplateColumns = `repeat(${arrayColumns}, 1fr)`;
+        lightContainer.style.gridTemplateRows = `repeat(${arrayRows}, 1fr)`;
+        for (let lightRowIndex = 0; lightRowIndex < arrayRows; lightRowIndex++) {
+            this.state[lightRowIndex] = [];
+            for (let lightColumnIndex = 0; lightColumnIndex < arrayColumns; lightColumnIndex++) {
                 const flashingLightContainer = document.createElement("div");
-                lightColumn.id = `flashing-light-${lightRowIndex}-${lightColumnIndex}`;
-                flashingLightContainer.id = lightColumn.id;
+                flashingLightContainer.id = `flashing-light-${lightRowIndex}-${lightColumnIndex}`;
                 flashingLightContainer.classList.add("flashing-light-container");
                 const flashingLight = document.createElement("div");
                 flashingLight.classList.add("light", "absolute-center");
-                flashingLight.addEventListener("animationend", function (event) {
-                    lightColumn.reset();
-                });
                 flashingLightContainer.appendChild(flashingLight);
                 lightContainer.appendChild(flashingLightContainer);
-                lightColumn.element = flashingLight;
-                lightColumnIndex++;
+                this.state[lightRowIndex].push(new LightSettings(flashingLight));
+                flashingLight.addEventListener("animationend", this.state[lightRowIndex][lightColumnIndex].reset.bind(this.state[lightRowIndex][lightColumnIndex]));
             }
-            lightRowIndex++;
         }
     }
 }
 let lightArray;
 
 window.onload = function () {
-    lightArray = new LightArray();
+    lightArray = new LightArray("light-container", 10, 10);
+    console.log(lightArray);
 
     setTimeout(() => {
-        alternating(5, .1);
+        alternating(5, .1, "flashing-light");
         setTimeout(() => {
             topLeftToBottomRight();
             setTimeout(() => {
@@ -69,14 +59,14 @@ window.onload = function () {
 }
 
 function topLeftToBottomRight() {
-    selectRows(true, "1", .1, .1, true, .05);
-    selectRows(false, "1", .1, .1, true, .05);
+    selectRows(true, "1", .1, "flashing-light", .1, true, .05,);
+    selectRows(false, "1", .1, "flashing-light", .1, true, .05);
 }
 
-function selectRows(even, iterationCount, duration, delay = 0, delayBasedOnIndex = false, delayMultiplier = 1, animationName = "flashing-light") {
+function selectRows(even, iterationCount, duration, animationName, delay = 0, delayBasedOnIndex = false, delayMultiplier = 1,) {
     for (let lightRowIndex = 0; lightRowIndex < lightArray.state.length; lightRowIndex++) {
         for (let lightColumnIndex = 0; lightColumnIndex < lightArray.state[lightRowIndex].length; lightColumnIndex++) {
-            if ((even && lightRowIndex % 2 == 0) || !even && lightRowIndex % 2 == 1) {
+            if ((even && lightRowIndex % 2 == 0) || (!even && lightRowIndex % 2 == 1)) {
                 if (delayBasedOnIndex) {
                     delay = (delay + lightRowIndex + lightColumnIndex + 1) * delayMultiplier;
                 }
@@ -86,7 +76,7 @@ function selectRows(even, iterationCount, duration, delay = 0, delayBasedOnIndex
     }
 }
 
-function alternating(iterationCount, duration, delay = 0, delayBasedOnIndex = false, animationName = "flashing-light") {
+function alternating(iterationCount, duration, animationName, delay = 0, delayBasedOnIndex = false) {
     for (let lightRowIndex = 0; lightRowIndex < lightArray.state.length; lightRowIndex++) {
         for (let lightColumnIndex = 0; lightColumnIndex < lightArray.state[lightRowIndex].length; lightColumnIndex++) {
             if (delayBasedOnIndex) {
