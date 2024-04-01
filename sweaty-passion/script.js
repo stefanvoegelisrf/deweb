8bec0d75e3f36d63835c7d247f89b3b9772bb748
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-04-02 00:43:51 +0200

    change images for sweaty passion

--- sweaty-passion/script.js
+++ sweaty-passion/script.js
@@ -1,15 +1,31 @@
+const images = [
+    "20220327_ltx2-1.png",
+]
 let lightArray;
 
 window.onload = function () {
-    lightArray = new LightArray("container", 10, 10);
+    lightArray = new LightArray("container", 5, 5);
+    changeImage();
+    setInterval(function () {
+        changeImage();
+        topLeftToBottomRight();
+    }, 1000);
 }
 
 function topLeftToBottomRight() {
-    lightArray.selectRowsFromBeginning(true, "2", .1, "flashing-light,light-magenta", .1, true, .05,);
-    lightArray.selectRowsFromBeginning(false, "2", .1, "flashing-light,light-cyan", .1, true, .05);
+    lightArray.selectRowsFromBeginning(true, "2", .1, "flashing-light,light-white", .1, true, .05,);
+    lightArray.selectRowsFromBeginning(false, "2", .1, "flashing-light,light-white", .1, true, .05);
 }
 
 function bottomRightToTopLeft() {
-    lightArray.selectRowsFromEnd(true, "2", .1, "flashing-light,light-magenta", .1, true, .05);
-    lightArray.selectRowsFromEnd(false, "2", .1, "flashing-light ,light-cyan", .1, true, .05);
+    lightArray.selectRowsFromEnd(true, "2", .1, "flashing-light,light-white", .1, true, .05);
+    lightArray.selectRowsFromEnd(false, "2", .1, "flashing-light ,light-white", .1, true, .05);
+}
+
+function changeImage() {
+    const imagePreload = document.getElementById("image-preload");
+    const images = imagePreload.querySelectorAll("img");
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const imageContainer = document.getElementById("image-container");
+    imageContainer.innerHTML = images[randomIndex].outerHTML;
 }
