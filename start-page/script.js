d1dc69397a6ccfe52f679ebd0a4804d26608371c
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-04-02 13:04:40 +0200

    remove number, add delayed navigation, add transition

--- start-page/script.js
+++ start-page/script.js
@@ -5,4 +5,15 @@ window.onload = function () {
         cursor.style.top = event.clientY + "px";
         cursor.style.opacity = 1;
     });
+
+    document.querySelectorAll('a').forEach((link) => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault();
+            const targetUrl = link.getAttribute('href');
+            link.classList.add("link-activated");
+            setTimeout(() => {
+                window.location.href = targetUrl;
+            }, 750);
+        });
+    });
 }
