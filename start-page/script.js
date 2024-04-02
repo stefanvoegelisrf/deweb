3d16200c75c6f0cdf0e0de747d0728623f0ec45e
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-04-02 22:08:06 +0200

    add hover for cursor

--- start-page/script.js
+++ start-page/script.js
@@ -4,6 +4,14 @@ window.onload = function () {
         cursor.style.left = event.clientX + "px";
         cursor.style.top = event.clientY + "px";
         cursor.style.opacity = 1;
+        const name = document.querySelector("#name");
+        const nameBoundingRect = name.getBoundingClientRect();
+        // Check if cursor is inside name
+        if (event.clientX > nameBoundingRect.left && event.clientX < nameBoundingRect.right && event.clientY > nameBoundingRect.top && event.clientY < nameBoundingRect.bottom) {
+            cursor.classList.add("cursor-inside-name");
+        } else {
+            cursor.classList.remove("cursor-inside-name");
+        }
     });
 
     document.querySelectorAll('a').forEach((link) => {
