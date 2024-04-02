2886e2fb8e8f0e010109f997822d8240b7345849
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-04-03 01:02:48 +0200

    adjust skate

--- skate-scrolly/script.js
+++ skate-scrolly/script.js
@@ -15,7 +15,41 @@ lenis.on('scroll', (e) => {
         rotation = map(Math.min(window.innerHeight * 0.5, e.animatedScroll), 0, window.innerHeight * 0.5, 0, 180);
     }
     document.documentElement.style.setProperty("--skate-rotation", `-${rotation}deg`);
+
+    // Map scrolled from 0 to 100 to 255 and 0
+    const color1 = map(scrolled, 0, 100, 255, 100);
+    const color2 = map(scrolled, 0, 100, 0, 200);
+    const color3 = map(scrolled, 0, 100, 255, 200);
+    document.documentElement.style.setProperty('--background-color', `rgb(${color1}, ${color2}, ${color3})`);
+
+    // Set transform to 50% if scrolled is between 50 and 60
+    // Map scrolled from 0 to -70
+    let transformX = 0;
+
+    if (e.direction === -1) {
+        transformX *= -1;
+    }
+    document.documentElement.style.setProperty('--skate-transform-x', `${transformX}%`);
+
 });
+
+const colors = ["cyan", "magenta", "yellow"]
+
+function createRandomStone(maxX, maxY) {
+    const stone = document.createElement('div');
+    stone.style.backgroundColor = colors[Math.floor(Math.random() * colors.length)];
+    stone.style.left = `${Math.random() * maxX}px`;
+    stone.style.top = `${Math.random() * maxY}px`;
+    const size = Math.random() * 2;
+    stone.style.width = `${size}rem`;
+    stone.style.height = `${size}rem`;
+    const borderRadius = Math.max(size / 2, Math.random() * 1);
+    stone.style.borderRadius = `${borderRadius}rem`;
+    stone.style.transform = `rotate(${Math.random() * 360}deg) translate(-50%, -50%)`;
+    stone.style.mixBlendMode = 'lighten';
+    stone.style.position = 'absolute';
+    return stone;
+}
 
 // Map function
 function map(value, start1, stop1, start2, stop2) {
@@ -29,17 +63,6 @@ function raf(time) {
 
 requestAnimationFrame(raf);
 
-function onScroll() {
-    // Get the scroll position
-    const scroll = window.scrollY;
-    // Set the scroll position as a css variable to the html element
-    // Calculate the percentage of scroll
-
-}
-
-// Add scroll event
-window.addEventListener('scroll', onScroll);
-
 function showAttributions() {
     const attributions = document.getElementById('attributions');
     attributions.classList.contains('show-attributions') ? attributions.classList.remove('show-attributions') : attributions.classList.toggle('show-attributions');
@@ -49,4 +72,13 @@ function showAttributions() {
 
 window.onload = function () {
     document.getElementById('attributions-title').addEventListener('click', showAttributions);
+    const stoneSections = document.querySelectorAll('.section-xl');
+    for (let section of stoneSections) {
+        const sectionBoundingRect = section.getBoundingClientRect();
+        const amountOfStones = Math.floor(Math.random() * 200);
+        const randomStones = new Array(amountOfStones).fill(null).map(() => createRandomStone(sectionBoundingRect.width, sectionBoundingRect.height));
+        for (let stone of randomStones) {
+            section.appendChild(stone);
+        }
+    }
 }
