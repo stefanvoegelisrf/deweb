2efe7e5dde704f95f89a51d348ad059235780fb6
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-04-02 23:06:19 +0200

    refine kinetic canvas

--- codingtrain-matterjs-introduction/sketch.js
+++ codingtrain-matterjs-introduction/sketch.js
@@ -11,31 +11,34 @@ var boxes = [];
 var ground;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(window.innerWidth * 0.5, window.innerHeight * 0.5);
     engine = Engine.create();
     world = engine.world;
     Runner.run(engine);
-    ground = Bodies.rectangle(200, height, width, 20, { isStatic: true });
+    ground = Bodies.rectangle(width / 2, height, width, 20, { isStatic: true });
     Composite.add(world, ground);
 }
 
+const colors = ["cyan", "magenta", "yellow"];
+
 function mousePressed() {
-    let newBox = new CustomBox(mouseX, mouseY, random(20, 40), random(20, 40));
+    let newBox = new CustomBox(mouseX, mouseY, random(20, 100), random(20, 100), colors[Math.floor(random(0, colors.length))]);
     boxes.push(newBox);
 }
 
 function draw() {
-    background(220);
+    background(0);
     for (let customBox of boxes) {
         customBox.show();
     }
 }
 
 class CustomBox {
-    constructor(x, y, width, height) {
+    constructor(x, y, width, height, color) {
         this.body = Bodies.rectangle(x, y, width, height, { friction: 0.5, restitution: 1, airFriction: 0 });
         this.width = width;
         this.height = height;
+        this.color = color;
         Composite.add(world, this.body);
     }
     show = function () {
@@ -45,6 +48,9 @@ class CustomBox {
         translate(pos.x, pos.y);
         rotate(angle);
         rectMode(CENTER);
+        fill(this.color);
+        stroke(255);
+        strokeWeight(2);
         rect(0, 0, this.width, this.height);
         pop();
 
