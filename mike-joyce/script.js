878ff29e0f96b5fc6924aa64fe66ab6c8b6bb7fd
Stefan Vögeli <stefan.voegeli@srf.ch>
2024-04-06 00:06:11 +0200

    add music

--- mike-joyce/script.js
+++ mike-joyce/script.js
@@ -1,6 +1,6 @@
 window.onload = function () {
     const cells = document.querySelectorAll('.cell');
     for (let i = 0; i < cells.length; i++) {
-        cells[i].style.animationDelay = `${i * 0.1}s`
+        cells[i].style.animationDelay = `${i * 0.015}s`
     }
 }
